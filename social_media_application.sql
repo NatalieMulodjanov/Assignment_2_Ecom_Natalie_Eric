8d71a3ae1f067abfaffbe85f6c0df8105127f209
NatalieM <Ntaliemulodjanov@gmail.com>
2021-10-12 17:40:55 -0400

    add htaccess, move things around

---
+++ social_media_application.sql
@@ -0,0 +1,208 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 12, 2021 at 11:24 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `social_media_application`
+--
+CREATE DATABASE IF NOT EXISTS `social_media_application` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `social_media_application`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message` (
+  `message_id` int(10) NOT NULL,
+  `sender` int(10) NOT NULL,
+  `receiver` int(10) NOT NULL,
+  `message` text NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `read_status` enum('read','unread','to_reread') NOT NULL,
+  `private_status` enum('public','private') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `picture`
+--
+
+DROP TABLE IF EXISTS `picture`;
+CREATE TABLE `picture` (
+  `picture_id` int(10) NOT NULL,
+  `profile_id` int(10) NOT NULL,
+  `file_name` varchar(20) NOT NULL,
+  `caption` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `picture_like`
+--
+
+DROP TABLE IF EXISTS `picture_like`;
+CREATE TABLE `picture_like` (
+  `picture_id` int(10) NOT NULL,
+  `profile_id` int(10) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `read_status` enum('seen','unseen') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+DROP TABLE IF EXISTS `profile`;
+CREATE TABLE `profile` (
+  `profile_id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `first_name` varchar(25) NOT NULL,
+  `middle_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` int(10) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password_hash` varchar(72) NOT NULL,
+  `2_factor_authentication_token` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `password_hash`, `2_factor_authentication_token`) VALUES
+(3, 'natalie', 'test', 'test'),
+(5, 'test', 'test', 'test');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`message_id`),
+  ADD KEY `sender` (`sender`),
+  ADD KEY `receiver` (`receiver`);
+
+--
+-- Indexes for table `picture`
+--
+ALTER TABLE `picture`
+  ADD PRIMARY KEY (`picture_id`),
+  ADD KEY `profile_id` (`profile_id`);
+
+--
+-- Indexes for table `picture_like`
+--
+ALTER TABLE `picture_like`
+  ADD KEY `profile_id` (`profile_id`),
+  ADD KEY `picture_id` (`picture_id`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`profile_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `message_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `picture`
+--
+ALTER TABLE `picture`
+  MODIFY `picture_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `profile_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`sender`) REFERENCES `profile` (`profile_id`),
+  ADD CONSTRAINT `message_ibfk_2` FOREIGN KEY (`receiver`) REFERENCES `profile` (`profile_id`);
+
+--
+-- Constraints for table `picture`
+--
+ALTER TABLE `picture`
+  ADD CONSTRAINT `picture_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`);
+
+--
+-- Constraints for table `picture_like`
+--
+ALTER TABLE `picture_like`
+  ADD CONSTRAINT `picture_like_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`),
+  ADD CONSTRAINT `picture_like_ibfk_2` FOREIGN KEY (`picture_id`) REFERENCES `picture` (`picture_id`);
+
+--
+-- Constraints for table `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `profile_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
