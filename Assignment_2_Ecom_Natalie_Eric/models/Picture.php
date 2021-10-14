b9ceda2c45771fb54057cc56233e161572a7f497
NatalieM <Ntaliemulodjanov@gmail.com>
2021-10-14 11:27:45 -0400

    write getAll method of Picture model

--- Assignment_2_Ecom_Natalie_Eric/models/Picture.php
+++ Assignment_2_Ecom_Natalie_Eric/models/Picture.php
@@ -2,5 +2,21 @@
 namespace Assignment_2_Ecom_Natalie_Eric\models;
 
 class Picture extends \Assignment_2_Ecom_Natalie_Eric\core\Model{
+    public $picture_id;
+    public $profile_id;
+    public $file_name;
+    public $caption;
 
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function getAll(){
+        $SQL = 'SELECT * FROM picture';
+        $STMT = self::$_connection->query($SQL);
+        $STMT->setFetchMode(\PDO::FETCH_CLASS, '\\Assignment_2_Ecom_Natalie_Eric\\models\\Picture');
+        return $STMT->fetchAll();
+    }
+
+    
 }
