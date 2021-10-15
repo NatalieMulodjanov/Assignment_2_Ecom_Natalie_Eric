5f2246833bfeeb6e805f7921e099b8bf6d7f8bb3
NatalieM <Ntaliemulodjanov@gmail.com>
2021-10-15 16:54:44 -0400

    create profile view

---
+++ app/views/Profile/create.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Create a Profile</title>
+    </head>
+
+    <body>
+        <form action="" method ="post">
+            <label>Enter First Name</label><input type="text" name="first_name"> 
+            <label>Enter Middle Name</label><input type="text" name="middle_name">
+            <label>Enter Last Name</label><input type="text" name="last_name"> 
+            <input type="submit" name='action' value='Create Profile'>
+        </form>
+    </body>
+</html>
