a21614541aab26124ab7f79e33c57b899d1f7f91
Eric Xu <shiqingxuvanier@gmail.com>
2021-10-20 14:02:24 -0400

    Placeholder User Controller

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -3,13 +3,12 @@ namespace app\controllers;
 
 class User extends \app\core\Controller{
 
-	//no need to get all users
-	// public function index(){
-	// 	$myUser = new \app\models\User();
-	// 	$results = $myUser->getAll();
+	public function index(){
+		$myUser = new \app\models\User();
+		$results = $myUser->getAll();
 
-	// 	$this->view('User/index',$results);
-	// }
+		$this->view('Account/index',$results);
+	}
 
 	
 }
