c1421996394ce45ac398637dbdea35d747063268
Eric Xu <shiqingxuvanier@gmail.com>
2021-10-20 14:54:26 -0400

    adde login/register

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -3,12 +3,25 @@ namespace app\controllers;
 
 class User extends \app\core\Controller{
 
+	#[\app\filters\Login]
 	public function index(){
-		$myUser = new \app\models\User();
-		$results = $myUser->getAll();
-
-		$this->view('Account/index',$results);
+		$this->view('Account/login');
 	}
 
+	public function login(){
+		$this->view('Account/login');
+	}
+
+	public function register(){
+		if(isset($_POST['action']) && $_POST['password'] == $_POST['password_confirm']){
+			$user = new \app\models\User();
+			$user->username = $_POST['username'];
+			$user->password = $_POST['password'];
+			$user->insert();
+			header('location:/User/login');
+
+		}else 
+			$this->view('Account/register');
+	}
 	
 }
