7b878a46e14ee5b552cb3aa1e85215b655837093
NatalieM <Ntaliemulodjanov@gmail.com>
2021-10-26 23:40:46 -0400

    1. redirect to wall in login 2.fix redirection to login in register method

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -5,18 +5,20 @@ class User extends \app\core\Controller{
 
 	#[\app\filters\Login]
 	public function index(){
-		$this->view('Account/login');
+		//session_destroy();
+		header('location:' .BASE. 'Profile/login');
 	}
 
 	public function login(){
 		if(isset($_POST['action'])){
 			$user = new \app\models\User();
 			$user = $user->get($_POST['username']);
-
+			
 			if($user!=false && password_verify($_POST['password'], $user->password_hash)){
 				$_SESSION['user_id'] = $user->user_id;
 				$_SESSION['username'] = $user->username;
-				echo 'go to wall';
+
+				header('location:' .BASE. 'Profile/index');
 			}else{
 				$this->view('Account/login','Wrong username and password combination!');
 			}
@@ -31,7 +33,7 @@ class User extends \app\core\Controller{
 			$user->username = $_POST['username'];
 			$user->password = $_POST['password'];
 			$user->insert();
-			header('location:/User/login');
+			header('location:'. BASE .'User/login');
 
 		}else 
 			$this->view('Account/register');
