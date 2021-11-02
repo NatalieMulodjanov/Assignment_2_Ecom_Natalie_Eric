cea76c92d46b9b0027ed154905a0ab34db096dbe
Eric Xu <shiqingxuvanier@gmail.com>
2021-11-02 00:34:11 -0400

    added picture edit/delete

---
+++ app/views/Picture/edit.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Edit Picture</title>
+    </head>
+
+    <body>
+        <a href="<?=BASE?>Profile/index">Return</a>
+
+        <form action="" method ="post">
+            <label>Change Caption</label><input type="text" name="caption"><br>
+            <input type="submit" name='action' value='Edit Picture'>
+        </form>
+    </body>
+</html>
